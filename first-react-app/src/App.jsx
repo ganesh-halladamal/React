bfc2f0d418edb052f81d1b5ef74f6189482accb0
Ganesh Halladamal <ganeshhalladmal@gmail.com>
2024-09-01 20:09:13 +0530

    linear gredient

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,5 +1,6 @@
 // src/App.jsx
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Header from './Header';
 import HeroSection from './HeroSection';
 import Services from './Services';
@@ -8,6 +9,7 @@ import AboutUs from './AboutUs';
 import TestimonialSection from './TestimonialSection';
 import ContactUs from './ContactUs';
 
+ReactDOM.render(<App />, document.getElementById('root'));
 function App() {
   return (
     <div className="container">
