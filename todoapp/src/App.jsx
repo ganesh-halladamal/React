59349bfaa7b8839809c433387de49920c57e6f07
Ganesh Halladamal <ganeshhalladmal@gmail.com>
2024-09-08 12:47:48 +0530

    todo component

--- todoapp/src/App.jsx
+++ todoapp/src/App.jsx
@@ -1,6 +1,39 @@
-function App(){
-  return <center class='to-do container'>
-    <h1>TO DO App</h1>
-  </center>
+function App() {
+  return (
+    <center class="to-do container">
+      <h1>TO DO App</h1>
+
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">
+            <input type="text" placeholder="Enter your todo" />
+          </div>
+          <div class="col-4"><input type="date" /></div>
+          <div class="col-2"><button type="button" class="btn btn-success">Add</button></div>
+        </div>
+      </div>
+
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">
+           Buy a new car
+          </div>
+          <div class="col-4">10/09/2024</div>
+          <div class="col-2"><button type="button" class="btn btn-danger">Delete</button></div>
+        </div>
+      </div>
+
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">
+           outing with team
+          </div>
+          <div class="col-4">11/09/2024</div>
+          <div class="col-2"><button type="button" class="btn btn-danger">Delete</button></div>
+        </div>
+      </div>
+
+    </center>
+  );
 }
-export default App
+export default App;
